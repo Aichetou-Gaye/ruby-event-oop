48edff6d77ae3322903449ccc321db289c6888a4
ambroise66 <ambroisesanou70@gmail.com>
2025-10-20 23:36:58 +0000

    Mise à jour sur l'utlisateur
    Refactor User class to include display method and improve attribute definition.

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,26 @@
-# require 'pry'
+require 'pry'
+
 class User
-  attr_accessor :email, :age
+  # Définition des attributs
+  attr_accessor :email
+  attr_accessor :age
 
-  @@all_users = []
-
+  # Initialisation (méthode initialize)
   def initialize(email, age)
     @email = email
     @age = age
-    
-  end  
+  end
 
+  # Méthode d'affichage
+  def afficher
+    # Utilisation des méthodes d'accès (email, age) ou des variables d'instance (@email, @age)
+    puts "Bonjour le monde! Je suis #{self.email} et j'ai #{self.age} ans."
+  end
 end
 
-# binding.pry
+# Création de l'instance
+# La syntaxe est Classe.new(arguments)
+m = User.new("julie@email.com", 32)
+
+# Appel de la méthode
+m.afficher
