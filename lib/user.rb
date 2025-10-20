ebd4a06d6ad5614fc9f98a023cca3d339dfa6517
Aichetou-Gaye <aichetouahmedgaye@gmail.com>
2025-10-21 00:29:54 +0100

    first commit

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,15 @@
+# require 'pry'
 class User
+  attr_accessor :email, :age
+
+  @@all_users = []
+
+  def initialize(email, age)
+    @email = email
+    @age = age
+    
+  end  
 
 end
+
+# binding.pry
