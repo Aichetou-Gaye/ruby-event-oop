3a88bf1b2f50aae125ee949d446f3af8166fd076
Aichetou-Gaye <aichetouahmedgaye@gmail.com>
2025-10-21 23:11:30 +0100

    resolved exercices

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,47 @@
-class Event
+# frozen_string_literal: true
 
+class Event # rubocop:disable Style/Documentation
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)
+
+    @duration = duration
+
+    @title = title
+
+    @attendees = attendees
+  end
+
+  def postpone_24h
+    @start_date += 60 * 60 * 24
+  end
+
+  def end_date
+    @start_date + duration * 60
+  end
+
+  def is_past? # rubocop:disable Naming/PredicatePrefix
+    @start_date < Time.now
+  end
+
+  def is_future? # rubocop:disable Naming/PredicatePrefix
+    !is_past?
+  end
+
+  def is_soon? # rubocop:disable Naming/PredicatePrefix
+    @start_date < Time.now + 60 * 30
+  end
+
+  def to_s
+    puts ">Titre : #{@title}"
+
+    puts ">Date de début : #{@start_date}"
+
+    puts ">Durée : #{@duration} minutes"
+
+    print '>Invités :'
+
+    puts @attendees.join(', ')
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,27 @@
-require 'pry'
+# frozen_string_literal: true
 
-class User
-  attr_accessor :email, :age
+class User # rubocop:disable Style/Documentation
+  attr_accessor :age, :email
 
-  @@user_all = []
+  @@all_users = [] # rubocop:disable Style/ClassVars
+
   def initialize(email, age)
     @email = email
+
     @age = age
+
+    @@all_users << self
   end
 
-  def afficher
-    puts "Bonjour le monde! Je suis #{email} et j'ai #{age} ans."
+  def self.all
+    @@all_users
+  end
+
+  def self.find_by_email(email)
+    @@all_users.each do |user|
+      return user if user.email == email
+    end
+    puts "aucun utilisateur n'a cet email"
+    false
   end
 end
-
-m = User.new('julie@email.com', 32)
-
-m.afficher
